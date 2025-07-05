fd7eae8d35b4f5b0f3ed42e16b827fe990a351ab
bharath391 <bharathkumarmerigala@gmail.com>
2025-07-05 10:20:28 +0530

    news routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,30 @@
 // backend/server.js
 const express = require('express');
 const dotenv = require('dotenv');
+
 const cors = require('cors');
 const connectDB = require('./config/db');
-const errorHandler = require('./middlewares/error.middleware');
-
 dotenv.config();
-connectDB();
+const errorHandler = require('./middlewares/error.middleware');
+const newsRouter = require('./routes/news.route');
+
+
+// connectDB();
+
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Routes
-app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/articles', require('./routes/articleRoutes'));
-app.use('/api/users', require('./routes/userRoutes'));
-app.use('/api/bookmarks', require('./routes/bookmarkRoutes'));
-app.use('/api/notifications', require('./routes/notificationRoutes'));
+//Routes
+// app.use('/api/auth', userRouter);
+app.use('/api/news', newsRouter);
+// app.use('/api/users', require('./routes/userRoutes'));
+// app.use('/api/bookmarks', require('./routes/bookmarkRoutes'));
+// app.use('/api/notifications', require('./routes/notificationRoutes'));
 
 // Error Handler
-app.use(errorHandler);
+// app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
